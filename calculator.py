2ca1b0fcf2ea751c8633adc314ce6996c8a3402c
Muchai10 <wanjirumuchai10@gmail.com>
2024-12-05 16:29:52 +0300

    commit

--- calculator.py
+++ calculator.py
@@ -1,3 +1,22 @@
-a = input("Enter a number")
-b = input("Enter another number")
-c = input("Enter another number")
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = input("Enter a mathematical operator i.e (+ - * /): ")
+
+if c == '+':
+    result = a + b
+    print(f"{a} + {b} = {result}")
+elif c == '-':
+    result = a - b
+    print(f"{a} - {b} = {result}")
+elif c == '*':
+    result = a * b
+    print(f"{a} * {b} = {result}")
+elif c == '/':
+    if b == 0:
+        print("Error! Division by zero is not allowed")
+    else:
+        result = a / b
+        print(f"{a} / {b} = {result}")
+else:
+    print("Invalid operator")
+
